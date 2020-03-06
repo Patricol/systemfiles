527b20f5aae0fe4600c6bae510c1b5e475f71cb4
Patrick Collins <PatrickCP0@Gmail.com>
2020-03-05 16:57:22 -0800

    fix keyring so it is unlocked on login

--- etc/X11/xinit/xinitrc.d/99-systemfiles.sh
+++ etc/X11/xinit/xinitrc.d/99-systemfiles.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# https://wiki.archlinux.org/index.php/GNOME/Keyring#xinitrc_method
+eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
+export SSH_AUTH_SOCK
+
 # autostart vncconfig so that the clipboard works (vncconfig exits immediately in non-VNC sessions).
 vncconfig -nowin &

5bb9165445afc675d987689710044cfe6ba9ccfa
Patrick Collins <PatrickCP0@Gmail.com>
2023-09-07 13:48:40 -0600

    misc

--- etc/X11/xinit/xinitrc.d/99-systemfiles.sh
+++ etc/X11/xinit/xinitrc.d/99-systemfiles.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-# https://wiki.archlinux.org/index.php/GNOME/Keyring#xinitrc_method
-eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh)
-export SSH_AUTH_SOCK
+source /usr/local/bin/start-ssh-agent
 
 # autostart vncconfig so that the clipboard works (vncconfig exits immediately in non-VNC sessions).
 vncconfig -nowin &
